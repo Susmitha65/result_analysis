da257e3472dd8dff29830b84f515fad68e305c9b
Susmitha V <148872403+Susmitha65@users.noreply.github.com>
2024-10-23 13:53:01 +0530

    Update urls.py

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import csv_upload, download_excel
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('upload/', views.csv_upload, name='csv_upload'),
+    path('download_excel/', download_excel, name='download_excel'),
 ]
