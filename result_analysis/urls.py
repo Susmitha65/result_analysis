73286f4c68ec01c60bba699ea20e96f12c7ebc2f
Susmitha V <148872403+Susmitha65@users.noreply.github.com>
2024-10-23 13:53:49 +0530

    Update urls.py

--- result_analysis/urls.py
+++ result_analysis/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('upload/', views.csv_upload, name='csv_upload'),  # CSV upload page
+    path('analysis/', include('analysis.urls')),
 
     # Serve homepage
     
